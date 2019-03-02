e748d926bc4bab230ab2c7ec8e0ee537f892a404
jordanhudgens <jordan@devcamp.com>
2019-03-02 13:57:30 -0600

    Added starter typescript code

---
+++ typescript-primer/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var world = "world";
+function hello(word) {
+    if (word === void 0) { word = world; }
+    return "Hello " + world;
+}
+exports.hello = hello;
+console.log("hello", hello("you!"));
+//# sourceMappingURL=index.js.map
